01b79a85debec92681eaaad1ff384fbb89ff95da
KenjieDec <65448230+KenjieDec@users.noreply.github.com>
2022-05-08 05:18:58 +0700

    resize fix

--- face_colorization.py
+++ face_colorization.py
@@ -37,13 +37,11 @@ if __name__=='__main__':
         filename = os.path.basename(file)
         
         grayf = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-        dim = (grayf.shape[1], grayf.shape[0])
         grayf = cv2.cvtColor(grayf, cv2.COLOR_GRAY2BGR) # channel: 1->3
 
         colorf = facecolorizer.process(grayf)
         
-        grayf = cv2.resize(grayf, (0,0))
-        colorf = cv2.resize(colorf, dim)
+        colorf = cv2.resize(colorf, (grayf.shape[1], grayf.shape[0]))
 
         cv2.imwrite(os.path.join(outdir, '.'.join(filename.split('.')[:-1])+'.jpg'), grayf)
         cv2.imwrite(os.path.join(outdir, '.'.join(filename.split('.')[:-1])+'_COMP.jpg'), np.hstack((grayf, colorf)))
